d7087556598de4465e554603f3c2430a7fdf7c17
lucasafonso-solutis <lucas.afonso@solutis.com.br>
2026-09-04 08:35:11 -0300

    fix: warning wrong import

--- src/test/java/solutis/lucas/afonso/helpdesk/services/NotificationServiceTest.java
+++ src/test/java/solutis/lucas/afonso/helpdesk/services/NotificationServiceTest.java
@@ -1,7 +1,6 @@
 package solutis.lucas.afonso.helpdesk.services;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertSame;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.never;
 import static org.mockito.Mockito.verify;
